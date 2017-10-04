bdec71f95430dfd2a63b3c6bab609f451101b40b
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-10-04 11:56:04 -0700

    Fix build, switch to rollup for prod

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,35 @@
-const babel = require('rollup-plugin-babel')
+var babel = require('rollup-plugin-babel')
+
+// TODO: Fix removing glslify imports after compilation ...
+function stripGlslify () {
+  return {
+    name: 'strip-glslify',
+    transform: function (code, id) {
+      var transformedCode = code.replace("import glsl from 'glslify'", '')
+      return {
+        code: transformedCode,
+        map: { mappings: '' }
+      }
+    }
+  }
+}
 
 module.exports = {
   plugins: [
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    stripGlslify()
+  ],
+  output: [
+    {
+      format: 'umd',
+      name: 'REGL',
+      file: 'dist/index.js'
+    },
+    {
+      format: 'es',
+      file: 'dist/index.module.js'
+    }
   ]
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,7 @@
-# TODO: Add es module build
 PATH=$(npm bin):$PATH
 export NODE_ENV=production
 
 rm -rf dist
 mkdir dist
-browserify -t [ rollupify --config rollup.config.js ] index.js > dist/index.js
+rollup --config rollup.config.js index.js
 uglifyjs --compress --mangle -- dist/index.js > dist/index.min.js
