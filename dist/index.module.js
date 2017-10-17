4bf2edbb43b6828700c0db1f9561e10f2387f13c
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-10-17 11:46:36 -0700

    Update builds

--- dist/index.module.js
+++ dist/index.module.js
@@ -1,452 +1,4 @@
-let ARRAY_TYPE = (typeof Float32Array !== 'undefined') ? Float32Array : Array;
-
-function create$1() {
-  let out = new ARRAY_TYPE(6);
-  out[0] = 1;
-  out[1] = 0;
-  out[2] = 0;
-  out[3] = 1;
-  out[4] = 0;
-  out[5] = 0;
-  return out;
-}
-
-function clone$1(a) {
-  let out = new ARRAY_TYPE(6);
-  out[0] = a[0];
-  out[1] = a[1];
-  out[2] = a[2];
-  out[3] = a[3];
-  out[4] = a[4];
-  out[5] = a[5];
-  return out;
-}
-
-function identity$1(out) {
-  out[0] = 1;
-  out[1] = 0;
-  out[2] = 0;
-  out[3] = 1;
-  out[4] = 0;
-  out[5] = 0;
-  return out;
-}
-
-function set$1(out, a, b, c, d, tx, ty) {
-  out[0] = a;
-  out[1] = b;
-  out[2] = c;
-  out[3] = d;
-  out[4] = tx;
-  out[5] = ty;
-  return out;
-}
-
-function rotate$1(out, a, rad) {
-  let a0 = a[0], a1 = a[1], a2 = a[2], a3 = a[3], a4 = a[4], a5 = a[5];
-  let s = Math.sin(rad);
-  let c = Math.cos(rad);
-  out[0] = a0 *  c + a2 * s;
-  out[1] = a1 *  c + a3 * s;
-  out[2] = a0 * -s + a2 * c;
-  out[3] = a1 * -s + a3 * c;
-  out[4] = a4;
-  out[5] = a5;
-  return out;
-}
-
-function scale$1(out, a, v) {
-  let a0 = a[0], a1 = a[1], a2 = a[2], a3 = a[3], a4 = a[4], a5 = a[5];
-  let v0 = v[0], v1 = v[1];
-  out[0] = a0 * v0;
-  out[1] = a1 * v0;
-  out[2] = a2 * v1;
-  out[3] = a3 * v1;
-  out[4] = a4;
-  out[5] = a5;
-  return out;
-}
-
-function translate(out, a, v) {
-  let a0 = a[0], a1 = a[1], a2 = a[2], a3 = a[3], a4 = a[4], a5 = a[5];
-  let v0 = v[0], v1 = v[1];
-  out[0] = a0;
-  out[1] = a1;
-  out[2] = a2;
-  out[3] = a3;
-  out[4] = a0 * v0 + a2 * v1 + a4;
-  out[5] = a1 * v0 + a3 * v1 + a5;
-  return out;
-}
-
-function create$2() {
-  let out = new ARRAY_TYPE(9);
-  out[0] = 1;
-  out[1] = 0;
-  out[2] = 0;
-  out[3] = 0;
-  out[4] = 1;
-  out[5] = 0;
-  out[6] = 0;
-  out[7] = 0;
-  out[8] = 1;
-  return out;
-}
-
-function create$5() {
-  let out = new ARRAY_TYPE(3);
-  out[0] = 0;
-  out[1] = 0;
-  out[2] = 0;
-  return out;
-}
-
-function length$1(a) {
-  let x = a[0];
-  let y = a[1];
-  let z = a[2];
-  return Math.sqrt(x*x + y*y + z*z);
-}
-
-function fromValues$5(x, y, z) {
-  let out = new ARRAY_TYPE(3);
-  out[0] = x;
-  out[1] = y;
-  out[2] = z;
-  return out;
-}
-
-function normalize$1(out, a) {
-  let x = a[0];
-  let y = a[1];
-  let z = a[2];
-  let len = x*x + y*y + z*z;
-  if (len > 0) {
-
-    len = 1 / Math.sqrt(len);
-    out[0] = a[0] * len;
-    out[1] = a[1] * len;
-    out[2] = a[2] * len;
-  }
-  return out;
-}
-
-function dot$1(a, b) {
-  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-
-function cross(out, a, b) {
-  let ax = a[0], ay = a[1], az = a[2];
-  let bx = b[0], by = b[1], bz = b[2];
-
-  out[0] = ay * bz - az * by;
-  out[1] = az * bx - ax * bz;
-  out[2] = ax * by - ay * bx;
-  return out;
-}
-
-const len$1 = length$1;
-
-const forEach = (function() {
-  let vec = create$5();
-
-  return function(a, stride, offset, count, fn, arg) {
-    let i, l;
-    if(!stride) {
-      stride = 3;
-    }
-
-    if(!offset) {
-      offset = 0;
-    }
-
-    if(count) {
-      l = Math.min((count * stride) + offset, a.length);
-    } else {
-      l = a.length;
-    }
-
-    for(i = offset; i < l; i += stride) {
-      vec[0] = a[i]; vec[1] = a[i+1]; vec[2] = a[i+2];
-      fn(vec, vec, arg);
-      a[i] = vec[0]; a[i+1] = vec[1]; a[i+2] = vec[2];
-    }
-
-    return a;
-  };
-})();
-
-function create$6() {
-  let out = new ARRAY_TYPE(4);
-  out[0] = 0;
-  out[1] = 0;
-  out[2] = 0;
-  out[3] = 0;
-  return out;
-}
-
-function normalize$2(out, a) {
-  let x = a[0];
-  let y = a[1];
-  let z = a[2];
-  let w = a[3];
-  let len = x*x + y*y + z*z + w*w;
-  if (len > 0) {
-    len = 1 / Math.sqrt(len);
-    out[0] = x * len;
-    out[1] = y * len;
-    out[2] = z * len;
-    out[3] = w * len;
-  }
-  return out;
-}
-
-const forEach$1 = (function() {
-  let vec = create$6();
-
-  return function(a, stride, offset, count, fn, arg) {
-    let i, l;
-    if(!stride) {
-      stride = 4;
-    }
-
-    if(!offset) {
-      offset = 0;
-    }
-
-    if(count) {
-      l = Math.min((count * stride) + offset, a.length);
-    } else {
-      l = a.length;
-    }
-
-    for(i = offset; i < l; i += stride) {
-      vec[0] = a[i]; vec[1] = a[i+1]; vec[2] = a[i+2]; vec[3] = a[i+3];
-      fn(vec, vec, arg);
-      a[i] = vec[0]; a[i+1] = vec[1]; a[i+2] = vec[2]; a[i+3] = vec[3];
-    }
-
-    return a;
-  };
-})();
-
-function create$4() {
-  let out = new ARRAY_TYPE(4);
-  out[0] = 0;
-  out[1] = 0;
-  out[2] = 0;
-  out[3] = 1;
-  return out;
-}
-
-function setAxisAngle(out, axis, rad) {
-  rad = rad * 0.5;
-  let s = Math.sin(rad);
-  out[0] = s * axis[0];
-  out[1] = s * axis[1];
-  out[2] = s * axis[2];
-  out[3] = Math.cos(rad);
-  return out;
-}
-
-function slerp(out, a, b, t) {
-
-  let ax = a[0], ay = a[1], az = a[2], aw = a[3];
-  let bx = b[0], by = b[1], bz = b[2], bw = b[3];
-
-  let omega, cosom, sinom, scale0, scale1;
-
-  cosom = ax * bx + ay * by + az * bz + aw * bw;
-
-  if ( cosom < 0.0 ) {
-    cosom = -cosom;
-    bx = - bx;
-    by = - by;
-    bz = - bz;
-    bw = - bw;
-  }
-
-  if ( (1.0 - cosom) > 0.000001 ) {
-
-    omega  = Math.acos(cosom);
-    sinom  = Math.sin(omega);
-    scale0 = Math.sin((1.0 - t) * omega) / sinom;
-    scale1 = Math.sin(t * omega) / sinom;
-  } else {
-
-    scale0 = 1.0 - t;
-    scale1 = t;
-  }
-
-  out[0] = scale0 * ax + scale1 * bx;
-  out[1] = scale0 * ay + scale1 * by;
-  out[2] = scale0 * az + scale1 * bz;
-  out[3] = scale0 * aw + scale1 * bw;
-
-  return out;
-}
-
-function fromMat3(out, m) {
-
-  let fTrace = m[0] + m[4] + m[8];
-  let fRoot;
-
-  if ( fTrace > 0.0 ) {
-
-    fRoot = Math.sqrt(fTrace + 1.0);
-    out[3] = 0.5 * fRoot;
-    fRoot = 0.5/fRoot;
-    out[0] = (m[5]-m[7])*fRoot;
-    out[1] = (m[6]-m[2])*fRoot;
-    out[2] = (m[1]-m[3])*fRoot;
-  } else {
-
-    let i = 0;
-    if ( m[4] > m[0] )
-      i = 1;
-    if ( m[8] > m[i*3+i] )
-      i = 2;
-    let j = (i+1)%3;
-    let k = (i+2)%3;
-
-    fRoot = Math.sqrt(m[i*3+i]-m[j*3+j]-m[k*3+k] + 1.0);
-    out[i] = 0.5 * fRoot;
-    fRoot = 0.5 / fRoot;
-    out[3] = (m[j*3+k] - m[k*3+j]) * fRoot;
-    out[j] = (m[j*3+i] + m[i*3+j]) * fRoot;
-    out[k] = (m[k*3+i] + m[i*3+k]) * fRoot;
-  }
-
-  return out;
-}
-
-const normalize = normalize$2;
-
-const rotationTo = (function() {
-  let tmpvec3 = create$5();
-  let xUnitVec3 = fromValues$5(1,0,0);
-  let yUnitVec3 = fromValues$5(0,1,0);
-
-  return function(out, a, b) {
-    let dot = dot$1(a, b);
-    if (dot < -0.999999) {
-      cross(tmpvec3, xUnitVec3, a);
-      if (len$1(tmpvec3) < 0.000001)
-        cross(tmpvec3, yUnitVec3, a);
-      normalize$1(tmpvec3, tmpvec3);
-      setAxisAngle(out, tmpvec3, Math.PI);
-      return out;
-    } else if (dot > 0.999999) {
-      out[0] = 0;
-      out[1] = 0;
-      out[2] = 0;
-      out[3] = 1;
-      return out;
-    } else {
-      cross(tmpvec3, a, b);
-      out[0] = tmpvec3[0];
-      out[1] = tmpvec3[1];
-      out[2] = tmpvec3[2];
-      out[3] = 1 + dot;
-      return normalize(out, out);
-    }
-  };
-})();
-
-const sqlerp = (function () {
-  let temp1 = create$4();
-  let temp2 = create$4();
-
-  return function (out, a, b, c, d, t) {
-    slerp(temp1, a, d, t);
-    slerp(temp2, b, c, t);
-    slerp(out, temp1, temp2, 2 * t * (1 - t));
-
-    return out;
-  };
-}());
-
-const setAxes = (function() {
-  let matr = create$2();
-
-  return function(out, view, right, up) {
-    matr[0] = right[0];
-    matr[3] = right[1];
-    matr[6] = right[2];
-
-    matr[1] = up[0];
-    matr[4] = up[1];
-    matr[7] = up[2];
-
-    matr[2] = -view[0];
-    matr[5] = -view[1];
-    matr[8] = -view[2];
-
-    return normalize(out, fromMat3(out, matr));
-  };
-})();
-
-function create$7() {
-  let out = new ARRAY_TYPE(2);
-  out[0] = 0;
-  out[1] = 0;
-  return out;
-}
-
-function copy$7(out, a) {
-  out[0] = a[0];
-  out[1] = a[1];
-  return out;
-}
-
-function set$7(out, x, y) {
-  out[0] = x;
-  out[1] = y;
-  return out;
-}
-
-function distance$2(a, b) {
-  var x = b[0] - a[0],
-    y = b[1] - a[1];
-  return Math.sqrt(x*x + y*y);
-}
-
-function transformMat2d(out, a, m) {
-  var x = a[0],
-    y = a[1];
-  out[0] = m[0] * x + m[2] * y + m[4];
-  out[1] = m[1] * x + m[3] * y + m[5];
-  return out;
-}
-
-const forEach$2 = (function() {
-  let vec = create$7();
-
-  return function(a, stride, offset, count, fn, arg) {
-    let i, l;
-    if(!stride) {
-      stride = 2;
-    }
-
-    if(!offset) {
-      offset = 0;
-    }
-
-    if(count) {
-      l = Math.min((count * stride) + offset, a.length);
-    } else {
-      l = a.length;
-    }
-
-    for(i = offset; i < l; i += stride) {
-      vec[0] = a[i]; vec[1] = a[i+1];
-      fn(vec, vec, arg);
-      a[i] = vec[0]; a[i+1] = vec[1];
-    }
-
-    return a;
-  };
-})();
+import { mat2d, vec2 } from 'gl-matrix';
 
 function setRGB (out, color) {
   var hexString = parseInt(color.replace(/^#/, ''), 16);
@@ -513,7 +65,7 @@ var CONTEXT_ACCESSORS = [
   'strokeStyle'
 ];
 
-var scratchVec2 = create$7();
+var scratchVec2 = vec2.create();
 
 function LineBuilder (regl, opts) {
   this.context = this.createContext(regl);
@@ -548,7 +100,7 @@ inherit(null, LineBuilder, {
     };
     var transform = {
       isIdentity: true,
-      matrix: create$1()
+      matrix: mat2d.create()
     };
     return {
       cursor: cursor,
@@ -556,7 +108,7 @@ inherit(null, LineBuilder, {
       style: style,
       transform: transform,
       activePath: null,
-      prevPosition: create$7(),
+      prevPosition: vec2.create(),
       paths: [],
       saveStack: []
     }
@@ -658,6 +210,18 @@ inherit(null, LineBuilder, {
     }
   },
 
+  combineDrawArgs: function (defaultArgs, userArgs) {
+    var args = Object.assign({}, defaultArgs);
+    Object.keys(userArgs).forEach(function (key) {
+      if (key === 'uniforms' || key === 'attributes') {
+        args[key] = Object.assign({}, defaultArgs[key], userArgs[key]);
+      } else {
+        args[key] = userArgs[key];
+      }
+    });
+    return args
+  },
+
   createDrawCommand: function (opts) {
     var attributes = this.attributes;
     var regl = this.context.regl;
@@ -684,9 +248,9 @@ inherit(null, LineBuilder, {
       return state.cursor.quad * 6
     };
 
-    var drawCommand = regl({
-      vert: opts.vert || line.vert,
-      frag: opts.frag || line.frag,
+    var defaultDrawArgs = {
+      vert: line.vert,
+      frag: line.frag,
       uniforms: uniforms,
       attributes: attributes,
       elements: resources.elements.buffer,
@@ -700,7 +264,12 @@ inherit(null, LineBuilder, {
           dst: 'one minus src alpha'
         }
       }
-    });
+    };
+    var drawArgs = opts.drawArgs
+      ? this.combineDrawArgs(defaultDrawArgs, opts.drawArgs)
+      : defaultDrawArgs;
+
+    var drawCommand = regl(drawArgs);
 
     return function (params) {
       if (state.sync.vertex < state.cursor.vertex) {
@@ -783,7 +352,7 @@ inherit(null, LineBuilder, {
     style.color[3] = 1;
 
     transform.isIdentity = true;
-    identity$1(transform.matrix);
+    mat2d.identity(transform.matrix);
 
     state.activePath = null;
     state.paths.length = 0;
@@ -801,7 +370,7 @@ inherit(null, LineBuilder, {
       },
       transform: {
         isIdentity: transform.isIdentity,
-        matrix: clone$1(transform.matrix)
+        matrix: mat2d.clone(transform.matrix)
       }
     });
   },
@@ -899,7 +468,7 @@ inherit(null, LineBuilder, {
     colorView[bib] = colorView[bib + 4] = color[2];
     colorView[bia] = colorView[bia + 4] = color[3];
 
-    copy$7(prevPosition, pos);
+    vec2.copy(prevPosition, pos);
     activePath.count += 1;
     cursor.vertex += 2;
   },
@@ -922,7 +491,7 @@ inherit(null, LineBuilder, {
     var elementsView = resources.elements.view;
 
     var pos = this.transformInput(x, y);
-    var segmentLength = distance$2(prevPosition, pos);
+    var segmentLength = vec2.distance(prevPosition, pos);
     var totalLength = activePath.totalLength += segmentLength;
 
     var aix = cursor.vertex * stride * 2;
@@ -961,7 +530,7 @@ inherit(null, LineBuilder, {
     elementsView[evi + 4] = bio;
     elementsView[evi + 5] = dio;
 
-    copy$7(prevPosition, pos);
+    vec2.copy(prevPosition, pos);
     activePath.count += 1;
     cursor.quad += 1;
     cursor.element += 2;
@@ -975,9 +544,9 @@ inherit(null, LineBuilder, {
 
     for (var i = 0; i < count; i++) {
       var t = i / (count - 1);
-      var angle$$1 = startAngle + t * delta * dir;
-      var ax = x + Math.cos(angle$$1) * radius;
-      var ay = y + Math.sin(angle$$1) * radius;
+      var angle = startAngle + t * delta * dir;
+      var ax = x + Math.cos(angle) * radius;
+      var ay = y + Math.sin(angle) * radius;
 
       if (i === 0) this.moveTo(ax, ay);
       else this.lineTo(ax, ay);
@@ -1138,37 +707,37 @@ inherit(null, LineBuilder, {
 
   setTransform: function (a, b, c, d, dx, dy) {
     var transform = this.state.transform;
-    set$1(transform.matrix, a, b, c, d, dx, dy);
+    mat2d.set(transform.matrix, a, b, c, d, dx, dy);
     transform.isIdentity = false;
   },
 
   translate: function (x, y) {
     var transform = this.state.transform;
-    var translation = set$7(scratchVec2, x, y);
-    translate(transform.matrix, transform.matrix, translation);
+    var translation = vec2.set(scratchVec2, x, y);
+    mat2d.translate(transform.matrix, transform.matrix, translation);
     transform.isIdentity = false;
   },
 
   scale: function (x, y) {
     var transform = this.state.transform;
-    var scale$$1 = set$7(scratchVec2, x, y);
-    scale$1(transform.matrix, transform.matrix, scale$$1);
+    var scale = vec2.set(scratchVec2, x, y);
+    mat2d.scale(transform.matrix, transform.matrix, scale);
     transform.isIdentity = false;
   },
 
-  rotate: function (angle$$1) {
+  rotate: function (angle) {
     var transform = this.state.transform;
-    rotate$1(transform.matrix, transform.matrix, angle$$1);
+    mat2d.rotate(transform.matrix, transform.matrix, angle);
     transform.isIdentity = false;
   },
 
   transformInput: function (x, y) {
     var activePath = this.state.activePath;
     var transform = this.state.transform;
-    var pos = set$7(scratchVec2, x, y);
+    var pos = vec2.set(scratchVec2, x, y);
 
     if (!transform.isIdentity && !activePath.isClosed) {
-      transformMat2d(pos, pos, transform.matrix);
+      vec2.transformMat2d(pos, pos, transform.matrix);
     }
     return pos
   }
