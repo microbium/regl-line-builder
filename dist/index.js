2b0a46934944c7dcb392124d440af8efb3984282
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-11-02 13:22:55 -0700

    Update builds

--- dist/index.js
+++ dist/index.js
@@ -68,6 +68,7 @@ var CONTEXT_ACCESSORS = [
   'lineWidth',
   'strokeStyle'
 ];
+var MAX_UINT16_INT = 65536;
 
 var scratchVec2 = glMatrix.vec2.create();
 
@@ -154,7 +155,6 @@ inherit(null, LineBuilder, {
     });
     var elementsBuffer = regl.elements({
       usage: 'dynamic',
-      type: 'uint16',
       primitive: 'triangles',
       data: views.elements
     });
@@ -184,13 +184,26 @@ inherit(null, LineBuilder, {
   },
 
   createResourceViews: function (size, stride) {
+    var ElementsArrayCtor = this.getElementsCtor(size);
     return {
       position: new Float32Array(size * stride * 2),
       offset: new Float32Array(size * 2),
       color: new Float32Array(size * 4 * 2),
       ud: new Float32Array(size * 2 * 3),
-      elements: new Uint16Array(size * 4)
+      elements: new ElementsArrayCtor(size * 4)
     }
+  },
+
+  getElementsCtor (size) {
+    var isBig = size * 4 * 2 > MAX_UINT16_INT;
+    var hasElementIndexExt = this.context.regl.hasExtension('OES_element_index_uint');
+    if (isBig && !hasElementIndexExt) {
+      throw new Error(
+        'bufferSize is too big for Uint16Array, ' +
+        'please enable OES_element_index_uint extension.')
+    }
+    return isBig && hasElementIndexExt
+      ? Uint32Array : Uint16Array
   },
 
   createAttributes: function () {
@@ -265,6 +278,10 @@ inherit(null, LineBuilder, {
       elements: resources.elements.buffer,
       count: count,
       depth: { enable: false },
+      cull: {
+        enable: true,
+        face: 'back'
+      },
       blend: {
         enable: true,
         equation: 'add',
