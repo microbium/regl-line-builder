5fcdea6e38f1926c744866ba10ffa43f38d03909
Jay Weeks <jay.patrick.weeks@gmail.com>
2018-04-13 11:43:21 -0700

    Hacky fix for glMatrix umd global exports

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -8,8 +8,17 @@ cleanup_lines () {
   mv $SRC.tmp $SRC
 }
 
+fix_glmat_umd_import () {
+  SRC=$1
+  GL_MAT_IMPORT="{vec2:global.vec2,vec3:global.vec3,mat2d:global.mat2d,mat4:global.mat4}"
+  cat $SRC | sed "s/global.glMatrix/$GL_MAT_IMPORT/g" > $SRC.tmp
+  rm $SRC
+  mv $SRC.tmp $SRC
+}
+
 rm -rf dist
 mkdir dist
 rollup $* --config rollup.config.js index.js
-cleanup_lines ./dist/regl-line-builder.js
-cleanup_lines ./dist/regl-line-builder.module.js
+cleanup_lines dist/regl-line-builder.js
+cleanup_lines dist/regl-line-builder.module.js
+fix_glmat_umd_import ./dist/regl-line-builder.js
